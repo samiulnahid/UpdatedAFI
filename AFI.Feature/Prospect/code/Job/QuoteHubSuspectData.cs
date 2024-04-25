7f42973ceec80e38a7b87c034dbc8e4bd07f88e9
samiulnahid <samiulislam212421@gmail.com>
2024-04-25 11:11:31 +0600

    updated

---
+++ AFI.Feature/Prospect/code/Job/QuoteHubSuspectData.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Data.SqlClient;
+using System.Configuration;
+using Sitecore.Data.Items;
+using AFI.Feature.Prospect.Models;
+using Dapper;
+using Sitecore.Diagnostics;
+
+namespace AFI.Feature.Prospect.Job
+{
+    public class QuoteHubSuspectData
+    {
+        private static readonly string AFIConnectionString = ConfigurationManager.ConnectionStrings["AFIDB"].ConnectionString;
+        private static readonly string QHConnectionString = ConfigurationManager.ConnectionStrings["QHAFIDB"].ConnectionString;
+
+
+        public void Execute(Item[] items, Sitecore.Tasks.CommandItem command, Sitecore.Tasks.ScheduleItem schedule)
+        {
+            Sitecore.Diagnostics.Log.Info("Qhote Hub Suspect Sitecore scheduled task is being run!", this);
+
+            List<QuoteHub> quotes = new List<QuoteHub>();
+
+            using (var db = new SqlConnection(QHConnectionString))
+            {
+                var sql = @"SELECT [QuoteHdr].*
+                            FROM [dbo].[QuoteHdr]
+                            LEFT JOIN [dbo].[PersonalInfo] ON [QuoteHdr].IdQuote = [PersonalInfo].IdQuote
+                            WHERE [PersonalInfo].IdQuote IS NULL;";
+
+                quotes = db.Query<QuoteHub>(sql).ToList();
+            }
+
+           
+
+            foreach (var data in quotes)
+            {
+                SuspectMarketingTemp entity = new SuspectMarketingTemp()
+                {
+
+                };
+
+                using (SqlConnection db = new SqlConnection(AFIConnectionString))
+                {
+                    db.Open();
+                    try
+                    {
+                        var sql = "insert into dbo.[AFI_Marketing_Suspect_Temp] ( [FirstName], [LastName], [Email], [Phone], [Address], [City], [State], [ZipCode], [Country], [DateOfBirth], [Occupation], [PreferredCoverage], [LeadSource], [LeadStatus], [LeadOwner], [LeadScore], [DateCreated], [LastUpdated], [EntityType], [EntityID], [IsSynced], [IsValid], [IsBlockCountry]) values (@FirstName, @LastName, @Email, @Phone, @Address, @City, @State, @ZipCode, @Country, @DateOfBirth, @Occupation, @PreferredCoverage, @LeadSource, @LeadStatus, @LeadOwner, @LeadScore, @DateCreated, @LastUpdated, @EntityType, @EntityID, @IsSynced, @IsValid, @IsBlockCountry)";
+
+                        using (SqlCommand cmd = new SqlCommand(sql, db))
+                        {
+                            cmd.Parameters.AddWithValue("@ListName", data.FirstName);
+                            cmd.Parameters.AddWithValue("@ListId", data.LastName);
+                            cmd.Parameters.AddWithValue("@CreatedDate", DateTime.Now);
+
+                            // Execute the query and retrieve the inserted ID
+                            cmd.ExecuteNonQuery();
+
+                           
+                        }
+                    }
+                    catch (Exception ex)
+                    {
+                        Log.Error("Error while attempting to insert Suspect Temp Data.", ex, this);
+                        // You may want to handle or log the exception accordingly
+                    }
+                    finally
+                    {
+                        db.Close();
+                    }
+                }
+            }
+
+        }
+
+    
+    }
+}
---
+++ AFI.Feature/Prospect/code/Models/QuoteHub.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace AFI.Feature.Prospect.Models
+{
+    public class QuoteHub
+    {
+        public int IdQuote { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public DateTime DOB { get; set; }
+        public string MailingAddr1 { get; set; }
+        public string MailingAddr2 { get; set; }
+        public string MailingCity { get; set; }
+        public string MailingState { get; set; }
+        public string MailingZip { get; set; }
+        public int ChipMemberId { get; set; }
+        public string EmailId { get; set; }
+        public string LOB { get; set; }
+        public int IdSuffix { get; set; }
+        public int CreatedBy { get; set; } = 1;
+        public DateTime CreatedOn { get; set; } = DateTime.Now;
+        public int ModifiedBy { get; set; }
+        public DateTime ModifiedOn { get; set; }
+        public string FNameSoundex { get; set; }
+        public string LNameSoundex { get; set; }
+        public string UserType { get; set; }
+        public string AgentId { get; set; }
+        public string AgentName { get; set; }
+        public string MailingCounty { get; set; }
+        public int IdMailingState { get; set; }
+        public string MailingStateName { get; set; }
+        public int IdMailingCountry { get; set; }
+        public string MailingCountryName { get; set; }
+        public string MailingCountryAbbrv { get; set; }
+        public string QuoteCreatedBy { get; set; }
+        public string QuoteModifiedBy { get; set; }
+        public bool IsTestAccount { get; set; } = false;
+        public int IdPrimarySalutation { get; set; }
+        public char PrimaryGender { get; set; }
+        public string OnePage { get; set; }
+    }
+}
